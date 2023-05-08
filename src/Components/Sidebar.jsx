fb2fb7cbd6b52d7fea9660341abc85a0e38a6b80
Lidhya <lidhyajeejan@gmail.com>
2023-05-08 22:20:13 +0530

    Added Link

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,6 +4,7 @@ import ArticleIcon from '@mui/icons-material/Article';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SettingsIcon from '@mui/icons-material/Settings';
 import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
@@ -13,26 +14,26 @@ const Sidebar = () => {
           DABLER
         </h5>
       </div>
-      <div className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
+      <Link to={"/"} className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
         <DashboardIcon />
         <span>Dashboard</span>
-      </div>
-      <div className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
+      </Link>
+      <Link to={"/projects"} className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
         <ArticleIcon />
         <span>Projects</span>
-      </div>
-      <div className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
+      </Link>
+      <Link to={"/"} className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
         <ImportContactsIcon />
         <span>Dictionary</span>
-      </div>
-      <div className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
+      </Link>
+      <Link to={"/"} className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
         <NotificationsIcon />
         <span>Incidents</span>
-      </div>  
-      <div className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
+      </Link>  
+      <Link to={"/"} className="hover:bg-light-blue p-4 w-full text-white flex gap-3 flex-wrap">
         <SettingsIcon />
         <span>Manage</span>
-      </div>
+      </Link>
     </div>
   );
 };
