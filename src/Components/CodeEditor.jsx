040a94205f11f0dc03c464922a3d5facab0f3ed1
Lidhya <lidhyajeejan@gmail.com>
2023-05-09 03:51:59 +0530

    Table data fetch added

---
+++ src/Components/CodeEditor.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const CodeEditor = ({onDataChange}) => {
+  const [code, setCode] = useState('');
+  
+  const handleChange = (e) => {
+    setCode(e.target.value);
+    onDataChange(e.target.value);
+  }
+
+  return (
+    <div className="flex flex-row bg-gray-100">
+      <div className="w-16 h-full flex flex-col justify-start items-center">
+        {Array.from({ length: 20 }).map((_, index) => (
+          <div key={index} className="text-gray-600 text-sm font-mono">{index + 1}</div>
+        ))}
+      </div>
+      <textarea
+        className="flex-1 bg-transparent outline-none p-2 font-mono text-sm resize-none"
+        value={code}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
