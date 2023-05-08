24e1aebe83a6bf17d5ee77e99646018c4117759e
Lidhya <lidhyajeejan@gmail.com>
2023-05-08 20:07:22 +0530

    Navbar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="my-3 bg-white w-full flex justify-between p-3 px-8">
+      <div className="flex gap-2 items-center justify-start flex-wrap">
+        <span className="font-bold text-2xl">Project1 Model1</span>
+        <span className="text-gray-400">|   Transformation Object</span>
+      </div>
+      <div className="flex text-white gap-4 mr-16 flex-wrap">
+        <button className="bg-blue-500 p-1 w-24">Save</button>
+        <button className="bg-red-400 p-1 w-24">Deploy</button>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
