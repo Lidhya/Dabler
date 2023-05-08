d7faa46d67121bfab54800e1928ec6da7d807478
Lidhya <lidhyajeejan@gmail.com>
2023-05-08 23:20:30 +0530

    Table ui completed

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const Table = () => {
-    return (
-        <div>
-            table
-        </div>
-    );
+function Table(props) {
+  const { data, headers } = props;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            {Object.values(row).map((value, index) => (
+              <td key={index}>{value}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
