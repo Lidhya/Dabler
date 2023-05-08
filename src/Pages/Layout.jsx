39d23419e7308c6292b2151bdb47312b6b54e12b
Lidhya <lidhyajeejan@gmail.com>
2023-05-08 22:21:23 +0530

    Pages added

---
+++ src/Pages/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Sidebar from '../Components/Sidebar';
+import { Outlet } from "react-router-dom";
+
+
+const Layout = () => {
+    return (
+        <div className="flex h-screen bg-off-white">
+        <div className="w-1/6 h-full bg-dark-grey">
+        <Sidebar/>
+        </div>
+        <div className="w-5/6 flex flex-col ">
+         <Outlet/>
+        </div>
+        </div>
+    );
+}
+
+export default Layout;
---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Tabs from '../Components/Tabs';
+
+const Projects = () => {
+    return (
+       <>
+        <div className='h-full w-full flex flex-col'>
+          <Navbar/>
+          <Tabs/>
+        </div>
+        </>
+    );
+}
+
+export default Projects;
